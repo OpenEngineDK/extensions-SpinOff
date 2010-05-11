d55a320912091e4ddd5f8633fb6b53686a4a9c67
asgerhoedt <asgerhoedt@gmail.com>
2010-05-11 14:09:39 +0000

    Spin the planet, princess, SPIN THE PLANET

--- Scene/MRINode.cpp
+++ Scene/MRINode.cpp
@@ -14,6 +14,8 @@
 
 #include <Utils/MeshCreator.h>
 
+#include <Logging/Logger.h>
+
 using namespace OpenEngine::Resources;
 using namespace OpenEngine::Utils::MeshCreator;
 
@@ -29,6 +31,32 @@ namespace OpenEngine {
             *verts -= Vector<3, float>(0,0,5);
 
             netMagnetization = Vector<3, float>(0,0,20);
+
+            microTime = 0;
+            
+            strengthB0 = 0.5;
+            microOmega0 = microGamma * strengthB0;
+        }
+
+        void MRINode::Handle(Core::ProcessEventArg arg){
+            microTime += arg.approx;
+
+            // Lagt ned vector
+            Vector<3, float> M0(20,0,0);
+
+            netMagnetization = StaticFieldEffect(M0, microTime);
+        }
+
+        Vector<3, float> MRINode::StaticFieldEffect(Vector<3, float> M0, unsigned int t){
+            Vector<3, float> ret = M0;
+            double cosToTime = cos(microOmega0 * t);
+            double sinToTime = sin(microOmega0 * t);
+            double T1exp = exp(-1 * double(-t)/double(T1));
+            double T2exp = exp(-1 * double(t)/double(T2));
+            ret[0] = T2exp * (M0[0] * cosToTime - M0[1] * sinToTime);
+            ret[1] = T2exp * (M0[0] * sinToTime - M0[1] * cosToTime);
+            ret[2] = M0[2] * T1exp + 40 * strengthB0 * (1-T1exp);
+            return ret;
         }
 
     }
--- Scene/MRINode.h
+++ Scene/MRINode.h
@@ -10,9 +10,12 @@
 #ifndef _MRI_NODE_H_
 #define _MRI_NODE_H_
 
-#include <Scene/ISceneNode.h>
-#include <boost/shared_ptr.hpp>
+#include <Core/EngineEvents.h>
+#include <Core/IListener.h>
 #include <Math/Vector.h>
+#include <Scene/ISceneNode.h>
+
+#include <boost/shared_ptr.hpp>
 
 using namespace OpenEngine::Math;
 
@@ -23,18 +26,38 @@ namespace OpenEngine {
     }
     namespace Scene {
 
-        class MRINode : public ISceneNode {
+        class MRINode : public ISceneNode,
+                        public Core::IListener<Core::ProcessEventArg>{
             OE_SCENE_NODE(MRINode, ISceneNode);
         public:
+
+            static const double T1 = 1e6; // spin lattice in microseconds.
+            static const double T2 = 1e6; // spin spin in microseconds.
+            static const double e = 2.718281828;
+            static const double gamma = 42.6e6; // hertz pr tesla
+            static const double microGamma = 42.6; // microhertz pr tesla
+
+            double strengthB0; // strength in tesla.
+            double microOmega0; // microhertz
 
             Geometry::MeshPtr northPole;
             Geometry::MeshPtr southPole;
             Vector<3, float> netMagnetization;
 
+            unsigned int microTime; // time in microseconds.
+
         public:
             MRINode();
 
-            
+            void Handle(Core::ProcessEventArg arg);
+
+        private:
+
+            /**
+             * The position of the vector mag at time t, influenced
+             * only by the static magnetic field B0.
+             */
+            Vector<3, float> StaticFieldEffect(Vector<3, float> mag, unsigned int t);
         };
 
     }
